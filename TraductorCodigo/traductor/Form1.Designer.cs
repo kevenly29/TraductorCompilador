ab77cf325e5df1d8409bae84b92bc62fa4f6f2b8
kevenly29 <56766471+kevenly29@users.noreply.github.com>
2023-03-05 21:00:10 -0400

    Traductor

---
+++ TraductorCodigo/traductor/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace ProyectoFinalLFP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.documentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generarTraduccionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generarReportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablaDeTokensToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablaDeSimbolosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablaDeErroresLexicosYSintacticosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.limpiarDocumentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnTraducir = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DarkCyan;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.documentoToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1164, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarComoToolStripMenuItem});
+            this.archivoToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            this.archivoToolStripMenuItem.Visible = false;
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.AbrirToolStripMenuItem_Click);
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarComoToolStripMenuItem.Text = "Guardar Como";
+            this.guardarComoToolStripMenuItem.Click += new System.EventHandler(this.GuardarComoToolStripMenuItem_Click);
+            // 
+            // documentoToolStripMenuItem
+            // 
+            this.documentoToolStripMenuItem.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.documentoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generarTraduccionToolStripMenuItem,
+            this.generarReportesToolStripMenuItem,
+            this.limpiarDocumentosToolStripMenuItem});
+            this.documentoToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.documentoToolStripMenuItem.Name = "documentoToolStripMenuItem";
+            this.documentoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.documentoToolStripMenuItem.Text = "Proceso";
+            this.documentoToolStripMenuItem.Visible = false;
+            // 
+            // generarTraduccionToolStripMenuItem
+            // 
+            this.generarTraduccionToolStripMenuItem.Name = "generarTraduccionToolStripMenuItem";
+            this.generarTraduccionToolStripMenuItem.Size = new System.Drawing.Size(235, 22);
+            this.generarTraduccionToolStripMenuItem.Text = "Generar Traduccion";
+            this.generarTraduccionToolStripMenuItem.Click += new System.EventHandler(this.GenerarTraduccionToolStripMenuItem_Click);
+            // 
+            // generarReportesToolStripMenuItem
+            // 
+            this.generarReportesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tablaDeTokensToolStripMenuItem,
+            this.tablaDeSimbolosToolStripMenuItem,
+            this.tablaDeErroresLexicosYSintacticosToolStripMenuItem});
+            this.generarReportesToolStripMenuItem.Name = "generarReportesToolStripMenuItem";
+            this.generarReportesToolStripMenuItem.Size = new System.Drawing.Size(235, 22);
+            this.generarReportesToolStripMenuItem.Text = "Generar Reportes";
+            // 
+            // tablaDeTokensToolStripMenuItem
+            // 
+            this.tablaDeTokensToolStripMenuItem.Name = "tablaDeTokensToolStripMenuItem";
+            this.tablaDeTokensToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
+            this.tablaDeTokensToolStripMenuItem.Text = "Tabla de Tokens";
+            this.tablaDeTokensToolStripMenuItem.Click += new System.EventHandler(this.tablaDeTokensToolStripMenuItem_Click);
+            // 
+            // tablaDeSimbolosToolStripMenuItem
+            // 
+            this.tablaDeSimbolosToolStripMenuItem.Name = "tablaDeSimbolosToolStripMenuItem";
+            this.tablaDeSimbolosToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
+            this.tablaDeSimbolosToolStripMenuItem.Text = "Tabla de simbolos";
+            this.tablaDeSimbolosToolStripMenuItem.Click += new System.EventHandler(this.tablaDeSimbolosToolStripMenuItem_Click);
+            // 
+            // tablaDeErroresLexicosYSintacticosToolStripMenuItem
+            // 
+            this.tablaDeErroresLexicosYSintacticosToolStripMenuItem.Name = "tablaDeErroresLexicosYSintacticosToolStripMenuItem";
+            this.tablaDeErroresLexicosYSintacticosToolStripMenuItem.Size = new System.Drawing.Size(263, 22);
+            this.tablaDeErroresLexicosYSintacticosToolStripMenuItem.Text = "Tabla de errores lexicos y sintacticos";
+            this.tablaDeErroresLexicosYSintacticosToolStripMenuItem.Click += new System.EventHandler(this.tablaDeErroresLexicosYSintacticosToolStripMenuItem_Click);
+            // 
+            // limpiarDocumentosToolStripMenuItem
+            // 
+            this.limpiarDocumentosToolStripMenuItem.Name = "limpiarDocumentosToolStripMenuItem";
+            this.limpiarDocumentosToolStripMenuItem.Size = new System.Drawing.Size(235, 22);
+            this.limpiarDocumentosToolStripMenuItem.Text = "Limpiar Documentos recientes";
+            this.limpiarDocumentosToolStripMenuItem.Click += new System.EventHandler(this.LimpiarDocumentosToolStripMenuItem_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            this.ayudaToolStripMenuItem.Visible = false;
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.AcercaDeToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.BackColor = System.Drawing.Color.Firebrick;
+            this.salirToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.salirToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.salirToolStripMenuItem.Visible = false;
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.SalirToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(15, 173);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(608, 334);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.White;
+            this.richTextBox2.Location = new System.Drawing.Point(687, 109);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(477, 522);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.Black;
+            this.richTextBox3.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox3.Location = new System.Drawing.Point(13, 537);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(610, 158);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 521);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(606, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "---Consola ----------------------------------------------------------------------" +
+    "--------------------------------------------------------------------------------" +
+    "---------------------------------";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(185, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = " C#";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(768, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 22);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Traducción a Phyton";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnTraducir
+            // 
+            this.btnTraducir.Location = new System.Drawing.Point(450, 130);
+            this.btnTraducir.Name = "btnTraducir";
+            this.btnTraducir.Size = new System.Drawing.Size(75, 23);
+            this.btnTraducir.TabIndex = 10;
+            this.btnTraducir.Text = "Traducir";
+            this.btnTraducir.UseVisualStyleBackColor = true;
+            this.btnTraducir.Click += new System.EventHandler(this.btnTraducir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1164, 707);
+            this.Controls.Add(this.btnTraducir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Traductor C# to Phyton";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem documentoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generarTraduccionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generarReportesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablaDeTokensToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablaDeSimbolosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablaDeErroresLexicosYSintacticosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem limpiarDocumentosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTraducir;
+    }
+}
+
